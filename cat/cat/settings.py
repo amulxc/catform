0794677ef1cf707f0791a1d78f8044caa9cbd2ce
amulxc <amul@xtracut.com>
2025-04-06 18:22:24 +0530

    Remove WhiteNoise middleware from settings.py and update STATIC_ROOT path to 'static' for consistency in static file management.

--- cat/cat/settings.py
+++ cat/cat/settings.py
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
   
 
 ]
@@ -128,7 +127,7 @@ USE_TZ = True
 
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Default primary key field type
