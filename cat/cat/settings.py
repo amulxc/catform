59f95b25183ba78dd486b0d6e9e8819a91e8407f
amulxc <amul@xtracut.com>
2025-04-06 17:16:10 +0530

    Set DEBUG to False in settings.py for production readiness and updated compiled settings.pyc file.

--- cat/cat/settings.py
+++ cat/cat/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l$42xa&#1jd_w)gx_n^_vraq7nh0u#*e6pa-w=gy_$q0r4^@k('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
